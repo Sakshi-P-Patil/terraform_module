cca07b05a9acce973b3feac2a9cfeacb170f869c
sakshi <patilsakshi7158@gmail.com>
2025-05-14 17:09:11 +0530

    terraform

--- instance/main_instance.tf
+++ instance/main_instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "terraform" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
-  security_groups = [ sg-070d43303c69203fa ]
+  security_groups = [ "sg-070d43303c69203fa"]
   
   tags = {
     Name = var.ins_name
