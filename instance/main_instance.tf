d273aa93e2ef7f736c03d8a99b5ab28420137086
sakshi <patilsakshi7158@gmail.com>
2025-05-14 17:24:48 +0530

    terraform

--- instance/main_instance.tf
+++ instance/main_instance.tf
@@ -1,9 +1,12 @@
 #create instance 
+resource "aws_security_group" "new_sec" {
+  
+}
 resource "aws_instance" "terraform" {
   ami = var.ami
   instance_type = var.instance_type
   key_name = var.key_name
-  security_groups = [ "sg-070d43303c69203fa"]
+  vpc_security_group_ids = [ "sg-070d43303c69203fa"]
   
   tags = {
     Name = var.ins_name
